16c6cdd8121d935b4f88eca808e2522860a2a7d5
AlSidorenko <alek.sidorenko1979@gmail.com>
2021-07-13 12:26:39 +0300

    Initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,12 +2,16 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        int variant_number = (16 - 1) % 10 + 1;
+        int k = ((3-1) % 10) + 1;
+        int a = (16 - ((16 - 1) % 10) - 1) / 10;
+
+        System.out.printf("Variant Number: %s\n", variant_number);
+        System.out.printf("K = %s\n", k);
+        System.out.printf("A = %s", a);
     }
 }
